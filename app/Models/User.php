6e4c014cbb5858634ef7d7b3acef1102399bb755
Raisyan <mraisyan365@gmail.com>
2024-09-19 15:51:59 +0700

    Memeperbaiki Role sama User

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'role_id', // Tambahkan role_id
+    
     ];
     
 
@@ -46,9 +46,6 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function role()
-{
-    return $this->belongsTo(Role::class);
 }
+    
 
-}
