a077b49cb2e794bb4ebbaa8209e81b10eda25b6d
Raisyan <mraisyan365@gmail.com>
2024-09-19 12:11:54 +0700

    add roles

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+        PermissionSeeder::class,
+        RoleSeeder::class,
+        UserSeeder::class,
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Redirect to login if user is not authenticated
+Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect('/dashboard');
+    }
+    return redirect('/login');
+});
+
+// Dashboard route, only accessible by authenticated users
+Route::get('/dashboard', function () {
+    return view('admin.blank.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+    // Route untuk Submenu 1
+Route::get('/submenu1', [AdminController::class, 'submenu1'])->name('submenu1');
+
+Route::resource('users', UserController::class);
+
+Route::resource('roles', RoleController::class);
+
+
+
+// Auth routes (login, register, password reset, etc.)
+require __DIR__.'/auth.php';
+
+
